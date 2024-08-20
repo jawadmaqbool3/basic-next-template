4f2a02504f520fb88edd894bf91ebb6671f8f691
Jawad Maqbool <Ahmedmaqbool@vizzweb.com>
2024-08-20 21:11:20 +0500

    changes

--- src/app/components/users/UserModal.tsx
+++ src/app/components/users/UserModal.tsx
@@ -28,30 +28,52 @@ const UserModal: React.FC<ModalComponentProps> = ({ openModal }) => {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">
-                Modal Title
-              </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                <div
+                  className="modal fade show"
+                  id="staticBackdropLive"
+                  data-bs-backdrop="static"
+                  data-bs-keyboard="false"
+                  tabIndex={-1}
+                  aria-labelledby="staticBackdropLiveLabel"
+                  aria-modal="true"
+                  role="dialog"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="staticBackdropLiveLabel">
+                          Modal title
+                        </h5>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">
+                        <p>
+                          I will not close if you click outside me. Don't even
+                          try to press escape key.
+                        </p>
+                      </div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" className="btn btn-primary">
+                          Understood
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </ModalBody>
-              <ModalFooter></ModalFooter>
             </>
           )}
         </ModalContent>
